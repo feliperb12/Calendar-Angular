1f0b5c55134fdb07501eb72eee6a69cb7ef5e527
feliperb12 <feliperibeiroaraujo3@hotmail.com>
2022-09-26 07:25:14 -0300

    Projeto Novo

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+
+import { FullCalendarModule } from 'primeng/fullcalendar';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+    CalendarComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FullCalendarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
